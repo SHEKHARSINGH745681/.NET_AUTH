8f4ae5c0eed678b3f38d3de1d0e7e4cc6a272807
Shekhar Singh <shekharsingh745681@gmail.com>
2024-11-04 11:11:39 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class PalindromeCheck
+{
+    public static bool IsPalindrome(string word)
+    {
+        //word ko lower case main kiya hai
+        word = word.ToLower();
+        return word == new string(word.Reverse().ToArray());
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter a word: ");
+        string word = Console.ReadLine();
+        Console.WriteLine(IsPalindrome(word) ? "It's a palindrome." : "Not a palindrome.");
+    }
+}
